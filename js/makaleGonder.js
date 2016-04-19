f640602cb4d7b93a8f9b48f0355eff04c2e18aea
Bugra <bugra9@users.noreply.github.com>
2016-04-19 03:09:13 +0300

    şifre alanı silindi

--- js/makaleGonder.js
+++ js/makaleGonder.js
@@ -39,7 +39,6 @@ $( document ).ready(function() {
       resim     : 'empty',
       yazar     : 'empty',
       eposta    : 'empty',
-      sifre     : 'empty',
       kural     : 'checked'
     },
     keyboardShortcuts: false
