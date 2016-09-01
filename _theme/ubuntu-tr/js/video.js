26851f73b3950bc72798e30e355e815bc82e0d95
bugra9 <bugra9@gmail.com>
2016-09-02 01:45:52 +0300

    Video yapısındaki hata giderildi

--- _theme/ubuntu-tr/js/video.js
+++ _theme/ubuntu-tr/js/video.js
@@ -1,6 +1,6 @@
 ---
 post: js
 ---
-var temp = $('a[href^="https://www.youtube.com/embed/"]')
-temp2 = $(temp).attr('href');
-$(temp)[0].outerHTML = '<div class="r16_9"><iframe src="'+temp2+'" frameborder="0" allowfullscreen> </iframe></div>';
+$('a[href^="https://www.youtube.com/embed/"]').each(function( index ) {
+	this.outerHTML = '<div class="r16_9"><iframe src="'+this.href+'" frameborder="0" allowfullscreen> </iframe></div>';
+});
